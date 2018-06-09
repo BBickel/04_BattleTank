17ad47328917a172873e76b2b6bca362efdfee3c
Bryant Bickel <gamedevbryant@gmail.com>
2018-06-09 10:37:30 -0500

    Section 4 Lecture 128 Override the BeginPlay function in TankPlayerController Add a log to output the Actor's name on Begin Play if not null

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,8 +3,25 @@
 #include "TankPlayerController.h"
 #include "Tank.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	ATank* thisTank = GetControlledTank();
+	//if (&thisTank == NULL)
+	if (!thisTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Tank Is NULL"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play: %s"), *(thisTank->GetName()));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
-
